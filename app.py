f879c8742eae9f9cd4f9b3304affeb3dea65e203
Milziade <you@example.com>
2021-05-02 22:17:08 +0200

    Delete test file, add table query & 404

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, redirect, url_for, request, render_template
 from form import LinkAddressForm
 from address_func import *
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '4e28a0ce03d1af568ddb0ea71be52fae30a5bab695dea817421e2a4fdb760014'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -32,44 +33,9 @@ def results(address):
                            overall_bal=overall_bal, time=time, insta_bal=insta_bal, colors=colors)
 
 
-labels = [
-    'JAN', 'FEB', 'MAR', 'APR',
-    'MAY', 'JUN', 'JUL', 'AUG',
-    'SEP', 'OCT', 'NOV', 'DEC'
-]
-
-values = [
-    967.67, 1190.89, 1079.75, 1349.19,
-    2328.91, 2504.28, 2873.83, 4764.87,
-    4349.29, 6458.30, 9907, 16297
-]
-
-colors = [
-    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
-    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
-    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
-
-
-@app.route('/bar')
-def bar():
-    bar_labels=labels
-    bar_values=values
-    return render_template('bar_chart.html', title='Bitcoin Monthly Price in USD', max=17000, labels=bar_labels, values=bar_values)
-
-
-@app.route('/line')
-def line():
-    line_labels=labels
-    line_values=values
-    return render_template('line_chart.html', title='Bitcoin Monthly Price in USD', max=17000, labels=line_labels, values=line_values)
-
-
-@app.route('/pie')
-def pie():
-    pie_labels = labels
-    pie_values = values
-    return render_template('pie_chart.html', title='Bitcoin Monthly Price in USD', max=17000, set=zip(values, labels, colors))
-
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=os.environ.get('DEBUG'))
---
+++ known_account.py
@@ -0,0 +1,3 @@
+NANO = {
+    'Burn': 'https://www.nanolooker.com/account/nano_1111111111111111111111111111111111111111111111111111hifc8npp'
+}
